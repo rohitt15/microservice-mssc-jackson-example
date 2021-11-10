bfc43973892294b41885b16cb8886c029f54c616
Rohit Kumar Singh <rohitt.singh15@gmail.com>
2021-11-10 11:23:51 +0530

    property-naming-strategies

---
+++ src/test/java/com/developer/msscjackson/model/BeerDtoSnakeTest.java
@@ -0,0 +1,26 @@
+package com.developer.msscjackson.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/*
+created by rohit on 10-11-21
+ */
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BestTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto beerDto=getDto();
+        String json=objectMapper.writeValueAsString(beerDto);
+        System.out.println(json);
+    }
+}
